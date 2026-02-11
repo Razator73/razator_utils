7d04b292e257927988142aef848d88ffaa31b69b
Ryan Scott <ryan.t.scott73@gmail.com>
2026-02-11 07:07:26 -0700

    Bump version: 0.2.0 → 0.3.0

--- razator_utils/__init__.py
+++ razator_utils/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Ryan Scott"""
 __email__ = 'ryan.t.scott73@gmail.com'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 from razator_utils.razator_utils import batchify, camel_to_snake, flatten_dict
 from razator_utils import log
